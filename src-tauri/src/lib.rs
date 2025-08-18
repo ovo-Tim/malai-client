d15978f2f0ce87cbb8c03b82875f14fe937e15d6
ovo-Tim <ovo-tim@qq.com>
2025-08-18 19:05:04 +0800

    feat(android): use notification to keep the app alive on android.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -99,11 +99,27 @@ fn parse_url(url: &str) -> eyre::Result<(&str, &str)> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![browse, status])
+        .invoke_handler(tauri::generate_handler![browse, status]);
+
+    #[cfg(mobile)]
+    let builder = builder.setup(|app| {
+        use tauri_plugin_notification::NotificationExt;
+        app.notification()
+            .builder()
+            .title("Malai client")
+            .body("I guess you need this to keep the app alive on android.")
+            .ongoing()
+            .show()
+            .unwrap();
+        Ok(())
+    });
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
